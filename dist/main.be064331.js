f31daa573bb3a6a203066a5d21b05e7d0d5689b2
Maximiliano Zavala <m.zavala22@tamu.edu>
2021-11-16 10:52:57 -0600

    Functioning base prototype

--- dist/main.be064331.js
+++ dist/main.be064331.js
@@ -110139,7 +110139,7 @@ function showAnimals(animals) {
     results.appendChild(div);
   });
 }
-},{"request":"node_modules/request/index.js"}],"../../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"request":"node_modules/request/index.js"}],"../../../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -110167,7 +110167,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49742" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65173" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -110343,5 +110343,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","javaS/main.js"], null)
+},{}]},{},["../../../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","javaS/main.js"], null)
 //# sourceMappingURL=/main.be064331.js.map
