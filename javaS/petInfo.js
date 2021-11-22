8cf74c7597ca89ffc5da52c401af467cef135a33
naimishkavani <naimishkavani@tamu.edu>
2021-11-21 22:48:25 -0600

    CAT API Integrated with the breeds showing on the drop down

---
+++ javaS/petInfo.js
@@ -0,0 +1,23 @@
+var request = require('request');
+var options = {
+  'method': 'GET',
+  'url': 'https://api.thecatapi.com/v1/breeds?api_key=71758f7c-16c4-42ca-af9c-ac83d9fb91df',
+  'headers': {
+  }
+};
+request(options, function (error, response) {
+  if (error) throw new Error(error);
+  console.log(response.body);
+  let infoPet = JSON.parse(response.body);
+  loadPetTypes(infoPet);
+});
+
+function loadPetTypes(petTypes) {
+  select = document.getElementById('animal');
+
+  petTypes.forEach((type) => {
+    option = document.createElement( 'option' );
+    option.value = option.text = type.name;
+    select.add(option);
+  });
+}
