139d5816f7513772b92c228224dc1c52fff68f60
naimishkavani <naimishkavani@tamu.edu>
2021-12-06 00:27:03 -0600

    Added contact informmation for pets up for adoption

--- dist/petInfo2.d219e361.js
+++ dist/petInfo2.d219e361.js
@@ -1115,7 +1115,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61053" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58072" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
